8d439a78ec3fbc177bc1af34023303ac6461c36b
kuzik <andrijkuzik@gmail.com>
2025-05-05 01:20:32 +0300

    feat: add basic repo structure

--- main.go
+++ main.go
@@ -1,56 +1,28 @@
 package main
 
 import (
+	"context"
 	"log"
-	"os"
 
+	"github.com/proofchronicle/content-indexer/config"
+	"github.com/proofchronicle/content-indexer/internal/consumer"
+	"github.com/proofchronicle/content-indexer/internal/svc"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-// failOnError logs and exits on error
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
-	}
-}
-
 func main() {
-	// RabbitMQ connection settings
-	rabbitURL := os.Getenv("RABBITMQ_URL")
-	if rabbitURL == "" {
-		failOnError(nil, "RABBITMQ_URL environment variable not set")
-	}
-	queueName := os.Getenv("QUEUE_NAME")
-	if queueName == "" {
-		failOnError(nil, "QUEUE_NAME environment variable not set")
-	}
+	config := config.NewConfig()
 
 	// Connect to RabbitMQ
-	conn, err := amqp.Dial(rabbitURL)
-	failOnError(err, "Failed to connect to RabbitMQ")
+	conn, err := amqp.Dial(config.RabbitMQURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %s", err)
+	}
 	defer conn.Close()
 
-	// Open a channel
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	// Consume messages
-	msgs, err := ch.Consume(
-		queueName, // queue
-		"",        // consumer
-		true,      // auto-ack
-		false,     // exclusive
-		false,     // no-local
-		false,     // no-wait
-		nil,       // args
-	)
-	failOnError(err, "Failed to register a consumer")
-
-	log.Printf("[*] Waiting for messages on queue '%s' (URL: %s)...", queueName, rabbitURL)
-
-	// Process messages
-	for d := range msgs {
-		log.Printf("[x] Received new message: %s", string(d.Body))
+	consumer, err := consumer.NewConsumer(conn, config.QueueName, svc.NewProcessor(*config))
+	if err != nil {
+		log.Fatalf("Failed to create consumer: %s", err)
 	}
+	consumer.Start(context.TODO())
 }
